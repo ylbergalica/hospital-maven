1ad3a6e77a919834e9c9ef7b8af3ffdbc247bc86
GalicaYlber <galica.ylber@gmail.com>
2024-01-14 17:36:18 +0200

    init

---
+++ src/city/org/rs/MyApplication.java
@@ -0,0 +1,19 @@
+package city.org.rs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+// @ApplicationPath("/") // Optional, specify the base path for your RESTful resources
+public class MyApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(AuthenticationFilter.class);
+        classes.add(CORSFilter.class);
+        // Add other resource classes if needed
+        return classes;
+    }
+}
