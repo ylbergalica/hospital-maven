3de11b4190bbd54eb8e44796c2948bdc9ffa08d0
GalicaYlber <galica.ylber@gmail.com>
2024-01-14 18:08:03 +0200

    pom and web xml changes

--- src/city/org/rs/MyApplication.java
+++ src/city/org/rs/MyApplication.java
@@ -5,7 +5,7 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-// @ApplicationPath("/") // Optional, specify the base path for your RESTful resources
+@ApplicationPath("/rest") // Optional, specify the base path for your RESTful resources
 public class MyApplication extends Application {
 
     @Override
